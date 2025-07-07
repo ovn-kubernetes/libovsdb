ovn-kubernetes/libovsdb

commit 15361fce1eb3101fc3d07a76b89910c294e36f2c

model: move methods from ClientModel to DatabaseModel
Now that client, cache and server uses the DatabaseModel as central
point of model creation and introspection, we can hide the
dbModelClient and move its public functions to the DatabaseModel

Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

model/client.go
@@ -15,7 +15,7 @@ type ClientDBModel struct {
 }
 
 // NewModel returns a new instance of a model from a specific string
-func (db ClientDBModel) NewModel(table string) (Model, error) {
+func (db ClientDBModel) newModel(table string) (Model, error) {
 	mtype, ok := db.types[table]
 	if !ok {
 		return nil, fmt.Errorf("table %s not found in database model", string(table))
@@ -24,32 +24,14 @@ func (db ClientDBModel) NewModel(table string) (Model, error) {
 	return model.Interface().(Model), nil
 }
 
-// Types returns the ClientDBModel Types
-// the ClientDBModel types is a map of reflect.Types indexed by string
-// The reflect.Type is a pointer to a struct that contains 'ovs' tags
-// as described above. Such pointer to struct also implements the Model interface
-func (db ClientDBModel) Types() map[string]reflect.Type {
-	return db.types
-}
-
 // Name returns the database name
 func (db ClientDBModel) Name() string {
 	return db.name
 }
 
-// FindTable returns the string associated with a reflect.Type or ""
-func (db ClientDBModel) FindTable(mType reflect.Type) string {
-	for table, tType := range db.types {
-		if tType == mType {
-			return table
-		}
-	}
-	return ""
-}
-
 // Validate validates the DatabaseModel against the input schema
 // Returns all the errors detected
-func (db ClientDBModel) Validate(schema *ovsdb.DatabaseSchema) []error {
+func (db ClientDBModel) validate(schema *ovsdb.DatabaseSchema) []error {
 	var errors []error
 	if db.name != schema.Name {
 		errors = append(errors, fmt.Errorf("database model name (%s) does not match schema (%s)",
@@ -62,7 +44,7 @@ func (db ClientDBModel) Validate(schema *ovsdb.DatabaseSchema) []error {
 			errors = append(errors, fmt.Errorf("database model contains a model for table %s that does not exist in schema", tableName))
 			continue
 		}
-		model, err := db.NewModel(tableName)
+		model, err := db.newModel(tableName)
 		if err != nil {
 			errors = append(errors, err)
 			continue

client/client.go
@@ -638,7 +638,7 @@ func (o *ovsdbClient) transact(ctx context.Context, dbName string, operation ...
 // MonitorAll is a convenience method to monitor every table/column
 func (o *ovsdbClient) MonitorAll(ctx context.Context) (MonitorCookie, error) {
 	m := newMonitor()
-	for name := range o.primaryDB().model.Client().Types() {
+	for name := range o.primaryDB().model.Types() {
 		m.Tables = append(m.Tables, TableMonitor{Table: name})
 	}
 	return o.Monitor(ctx, m)
@@ -696,7 +696,7 @@ func (o *ovsdbClient) monitor(ctx context.Context, cookie MonitorCookie, reconne
 	db.schemaMutex.RLock()
 	mapper := db.model.Mapper()
 	db.schemaMutex.RUnlock()
-	typeMap := o.databases[dbName].model.Client().Types()
+	typeMap := o.databases[dbName].model.Types()
 	requests := make(map[string]ovsdb.MonitorRequest)
 	for _, o := range monitor.Tables {
 		m, ok := typeMap[o.Table]

cache/cache.go
@@ -451,7 +451,7 @@ func NewTableCache(dbModel *model.DatabaseModel, data Data, logger *logr.Logger)
 	}
 	eventProcessor := newEventProcessor(bufferSize, logger)
 	cache := make(map[string]*RowCache)
-	tableTypes := dbModel.Client().Types()
+	tableTypes := dbModel.Types()
 	for name, tableSchema := range dbModel.Schema().Tables {
 		cache[name] = newRowCache(name, tableSchema, tableTypes[name])
 	}
@@ -482,9 +482,9 @@ func (t *TableCache) Mapper() *mapper.Mapper {
 	return t.dbModel.Mapper()
 }
 
-// ClientDBModel returns the ClientDBModel
-func (t *TableCache) ClientDBModel() *model.ClientDBModel {
-	return t.dbModel.Client()
+// DatabaseModel returns the DatabaseModelRequest
+func (t *TableCache) DatabaseModel() *model.DatabaseModel {
+	return t.dbModel
 }
 
 // Table returns the a Table from the cache with a given name
@@ -549,7 +549,7 @@ func (t *TableCache) Populate(tableUpdates ovsdb.TableUpdates) error {
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
 
-	for table := range t.dbModel.Client().Types() {
+	for table := range t.dbModel.Types() {
 		updates, ok := tableUpdates[table]
 		if !ok {
 			continue
@@ -600,7 +600,7 @@ func (t *TableCache) Populate(tableUpdates ovsdb.TableUpdates) error {
 func (t *TableCache) Populate2(tableUpdates ovsdb.TableUpdates2) error {
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
-	for table := range t.dbModel.Client().Types() {
+	for table := range t.dbModel.Types() {
 		updates, ok := tableUpdates[table]
 		if !ok {
 			continue
@@ -672,7 +672,7 @@ func (t *TableCache) Purge(dbModel *model.DatabaseModel) {
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
 	t.dbModel = dbModel
-	tableTypes := t.dbModel.Client().Types()
+	tableTypes := t.dbModel.Types()
 	for name, tableSchema := range t.dbModel.Schema().Tables {
 		t.cache[name] = newRowCache(name, tableSchema, tableTypes[name])
 	}
@@ -851,7 +851,7 @@ func (t *TableCache) CreateModel(tableName string, row *ovsdb.Row, uuid string) 
 	if table == nil {
 		return nil, fmt.Errorf("table %s not found", tableName)
 	}
-	model, err := t.dbModel.Client().NewModel(tableName)
+	model, err := t.dbModel.NewModel(tableName)
 	if err != nil {
 		return nil, err
 	}

client/api.go
@@ -280,7 +280,7 @@ func (a api) Mutate(model model.Model, mutationObjs ...model.Mutation) ([]ovsdb.
 		return nil, fmt.Errorf("at least one Mutation must be provided")
 	}
 
-	tableName := a.cache.ClientDBModel().FindTable(reflect.ValueOf(model).Type())
+	tableName := a.cache.DatabaseModel().FindTable(reflect.ValueOf(model).Type())
 	if tableName == "" {
 		return nil, fmt.Errorf("table not found for object")
 	}
@@ -414,7 +414,7 @@ func (a api) getTableFromModel(m interface{}) (string, error) {
 	if _, ok := m.(model.Model); !ok {
 		return "", &ErrWrongType{reflect.TypeOf(m), "Type does not implement Model interface"}
 	}
-	table := a.cache.ClientDBModel().FindTable(reflect.TypeOf(m))
+	table := a.cache.DatabaseModel().FindTable(reflect.TypeOf(m))
 	if table == "" {
 		return "", &ErrWrongType{reflect.TypeOf(m), "Model not found in Database Model"}
 	}
@@ -439,7 +439,7 @@ func (a api) getTableFromFunc(predicate interface{}) (string, error) {
 			fmt.Sprintf("Type %s does not implement Model interface", modelType.String())}
 	}
 
-	table := a.cache.ClientDBModel().FindTable(modelType)
+	table := a.cache.DatabaseModel().FindTable(modelType)
 	if table == "" {
 		return "", &ErrWrongType{predType,
 			fmt.Sprintf("Model %s not found in Database Model", modelType.String())}

client/monitor.go
@@ -70,7 +70,7 @@ type TableMonitor struct {
 
 func WithTable(m model.Model, fields ...interface{}) MonitorOption {
 	return func(o *ovsdbClient, monitor *Monitor) error {
-		tableName := o.primaryDB().model.Client().FindTable(reflect.TypeOf(m))
+		tableName := o.primaryDB().model.FindTable(reflect.TypeOf(m))
 		if tableName == "" {
 			return fmt.Errorf("object of type %s is not part of the ClientDBModel", reflect.TypeOf(m))
 		}
@@ -85,7 +85,7 @@ func WithTable(m model.Model, fields ...interface{}) MonitorOption {
 
 func WithConditionalTable(m model.Model, condition model.Condition, fields ...interface{}) MonitorOption {
 	return func(o *ovsdbClient, monitor *Monitor) error {
-		tableName := o.primaryDB().model.Client().FindTable(reflect.TypeOf(m))
+		tableName := o.primaryDB().model.FindTable(reflect.TypeOf(m))
 		if tableName == "" {
 			return fmt.Errorf("object of type %s is not part of the ClientDBModel", reflect.TypeOf(m))
 		}

model/database.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"fmt"
+	"reflect"
 	"sync"
 
 	"github.com/ovn-org/libovsdb/mapper"
@@ -17,9 +19,9 @@ type DatabaseModel struct {
 }
 
 // NewDatabaseModel returns a new DatabaseModel
-func NewDatabaseModel(schema *ovsdb.DatabaseSchema, request *ClientDBModel) *DatabaseModel {
+func NewDatabaseModel(schema *ovsdb.DatabaseSchema, client *ClientDBModel) *DatabaseModel {
 	return &DatabaseModel{
-		client: request,
+		client: client,
 		schema: schema,
 		mapper: mapper.NewMapper(schema),
 	}
@@ -43,7 +45,7 @@ func (db *DatabaseModel) Valid() bool {
 func (db *DatabaseModel) SetSchema(schema *ovsdb.DatabaseSchema) []error {
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
-	errors := db.client.Validate(schema)
+	errors := db.client.validate(schema)
 	if len(errors) > 0 {
 		return errors
 	}
@@ -77,4 +79,32 @@ func (db *DatabaseModel) Mapper() *mapper.Mapper {
 	db.mutex.RLock()
 	defer db.mutex.RUnlock()
 	return db.mapper
+}
+
+// NewModel returns a new instance of a model from a specific string
+func (db *DatabaseModel) NewModel(table string) (Model, error) {
+	mtype, ok := db.client.types[table]
+	if !ok {
+		return nil, fmt.Errorf("table %s not found in database model", string(table))
+	}
+	model := reflect.New(mtype.Elem())
+	return model.Interface().(Model), nil
+}
+
+// Types returns the DatabaseModel Types
+// the DatabaseModel types is a map of reflect.Types indexed by string
+// The reflect.Type is a pointer to a struct that contains 'ovs' tags
+// as described above. Such pointer to struct also implements the Model interface
+func (db *DatabaseModel) Types() map[string]reflect.Type {
+	return db.client.types
+}
+
+// FindTable returns the string associated with a reflect.Type or ""
+func (db *DatabaseModel) FindTable(mType reflect.Type) string {
+	for table, tType := range db.client.types {
+		if tType == mType {
+			return table
+		}
+	}
+	return ""
 }

model/model_test.go
@@ -53,7 +53,7 @@ func TestClientDBModel(t *testing.T) {
 			db, err := NewClientDBModel(tt.name, tt.obj)
 			if tt.valid {
 				assert.Nil(t, err)
-				assert.Len(t, db.Types(), len(tt.obj))
+				assert.Len(t, db.types, len(tt.obj))
 				assert.Equal(t, tt.name, db.Name())
 			} else {
 				assert.NotNil(t, err)
@@ -65,9 +65,9 @@ func TestClientDBModel(t *testing.T) {
 func TestNewModel(t *testing.T) {
 	db, err := NewClientDBModel("testTable", map[string]Model{"Test_A": &modelA{}, "Test_B": &modelB{}})
 	assert.Nil(t, err)
-	_, err = db.NewModel("Unknown")
+	_, err = db.newModel("Unknown")
 	assert.NotNilf(t, err, "Creating model from unknown table should fail")
-	model, err := db.NewModel("Test_A")
+	model, err := db.newModel("Test_A")
 	assert.Nilf(t, err, "Creating model from valid table should succeed")
 	assert.IsTypef(t, model, &modelA{}, "model creation should return the appropriate type")
 }
@@ -324,7 +324,7 @@ func TestValidate(t *testing.T) {
 			var schema ovsdb.DatabaseSchema
 			err := json.Unmarshal(tt.schema, &schema)
 			assert.Nil(t, err)
-			errors := model.Validate(&schema)
+			errors := model.validate(&schema)
 			if tt.err {
 				assert.Greater(t, len(errors), 0)
 			} else {

server/transact.go
@@ -82,7 +82,7 @@ func (o *OvsdbServer) Insert(database string, table string, rowUUID string, row 
 		rowUUID = uuid.NewString()
 	}
 
-	model, err := dbModel.Client().NewModel(table)
+	model, err := dbModel.NewModel(table)
 	if err != nil {
 		return ovsdb.OperationResult{
 			Error: err.Error(),
@@ -201,7 +201,7 @@ func (o *OvsdbServer) Update(database, table string, where []ovsdb.Condition, ro
 		if err != nil {
 			panic(err)
 		}
-		new, err := dbModel.Client().NewModel(table)
+		new, err := dbModel.NewModel(table)
 		if err != nil {
 			panic(err)
 		}
@@ -333,7 +333,7 @@ func (o *OvsdbServer) Mutate(database, table string, where []ovsdb.Condition, mu
 		if err != nil {
 			panic(err)
 		}
-		new, err := dbModel.Client().NewModel(table)
+		new, err := dbModel.NewModel(table)
 		if err != nil {
 			panic(err)
 		}

commit 4faeffc36e43dc9a61a94f12284cb4b2bd209aa1

model: add Types() back to ClientDBModel
The commit in the Fixes tag removed the Types() from the API.
The intent was to centralize the internal use of the model around the
DatabaseModel struct which contains both the ClientDBModel and the
Schema.

However, it breaks the public API in a way that is not easy to fix for
clients that used to use dbModel.Types() to access the Types that will
be used in libovsdb. An example of such clients is
https://github.com/amorenoz/ovsdb-mon  who uses the type information to
generate a things like filters and autocompletion information.

This commit adds the deleted API back to ClientDBModel to allow clients
an easy access of the internal typing information.

Fixes: 15361fce1eb3 ("model: move methods from ClientModel to DatabaseModel")
Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

model/client.go
@@ -36,6 +36,13 @@ func (db ClientDBModel) newModel(table string) (Model, error) {
 	return model.Interface().(Model), nil
 }
 
+// Types returns the ClientDBModel Types
+// The ClientDBModel types is a map of reflect.Types indexed by string
+// The reflect.Type is a pointer to a struct that contains 'ovs' tags.
+func (db ClientDBModel) Types() map[string]reflect.Type {
+	return db.types
+}
+
 // Name returns the database name
 func (db ClientDBModel) Name() string {
 	return db.name