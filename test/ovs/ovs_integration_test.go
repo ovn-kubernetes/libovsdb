0ea2461abfb530b25ded6ee1d9ef16a15fc472dc
Dave Tucker <dave@dtucker.co.uk>
2025-07-25 13:10:37 +0100

    test: Enable TestWithReconnect
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- test/ovs/ovs_integration_test.go
+++ test/ovs/ovs_integration_test.go
@@ -378,7 +378,6 @@ func (suite *OVSIntegrationSuite) TestWithInactivityCheck() {
 }
 
 func (suite *OVSIntegrationSuite) TestWithReconnect() {
-	suite.T().Skip("On container restart client is connected but rpc2 connection is shutdown, so Echo fails with ErrNotConnected")
 	suite.True(suite.clientWithoutInactvityCheck.Connected())
 	err := suite.clientWithoutInactvityCheck.Echo(context.TODO())
 	suite.Require().NoError(err)
